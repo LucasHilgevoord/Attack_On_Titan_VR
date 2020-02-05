0c75b9cc02083fa142f21c0760364ba18da76da9
DylanTheProgrammer <43721637+DylanTheProgrammer@users.noreply.github.com>
2020-02-05 03:04:52 +0100

    Advanced cameraFollower script
    Remember the basic cameraFollower from previous update? I've now updated it with a more advanced script. It rotates nicely and makes some corrections so the position doesn't get weird.
    The cameraFollower script in the body now has 4 customizable properties:
    - hipRatio: The higher this number, the lower the body will be, but it will maintain the same ratio to the current player height.
    - rotationRatio: Sets how much the body rotates along vertically.
    - neckRadius: Sets how big the correction in horizontal positioning should be according to head rotation.
    - forwardOffset: Sets the body a bit further so you can actually see it when you look down.

--- AttackOnTitan/Assets/Scripts/Player/CameraFollower/CameraFollower.cs
+++ AttackOnTitan/Assets/Scripts/Player/CameraFollower/CameraFollower.cs
@@ -11,9 +11,21 @@ public class CameraFollower : MonoBehaviour
     [SerializeField]
     float hipRatio = 2f;
 
-    float yOffset = 0;
+    [SerializeField]
+    float rotationRatio = 0.2f;
 
-    float yRot = 0f;
+    [SerializeField]
+    float forwardOffset = 0.2f;
+
+    [SerializeField]
+    float neckRadius = 0.1f;
+
+    float xOffset = 0f;
+    float yOffset = 0f;
+    float zOffset = 0f;
+
+    Vector3 forward;
+    Vector3 horizontal;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +36,24 @@ public class CameraFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Set the body at a certain ratio of your current height, so it will always be somewhere around your actual hips
         yOffset = VRCamera.transform.localPosition.y / hipRatio;
-        transform.position = new Vector3(VRCamera.position.x, VRCamera.parent.position.y + yOffset, VRCamera.position.z);
 
-        //Rotation following with quaternions
-        yRot = Quaternion.ToEulerAngles(VRCamera.rotation).y * 180 / Mathf.PI;
-        transform.rotation = Quaternion.Euler(0, yRot, 0);
+        //Set xOffset and zOffset according to sideways and forwards/backwards head rotation
+        xOffset = -VRCamera.up.x * neckRadius;
+        zOffset = -VRCamera.up.z * neckRadius;
+        transform.position = new Vector3(VRCamera.position.x + xOffset, VRCamera.parent.position.y + yOffset, VRCamera.position.z + zOffset);
+
+        //Complex but nice rotation. Body rotates with you on Y-axis and partially rotates vertically. It won't break when looking completely up or downwards.
+        forward = VRCamera.forward;
+        forward.x += VRCamera.up.x * -VRCamera.forward.y;
+        forward.z += VRCamera.up.z * -VRCamera.forward.y;
+        horizontal = forward;
+        horizontal.y = 0f;
+        forward.y = forward.y * rotationRatio * Vector3.Magnitude(horizontal);
+        transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+
+        //Now set the body a bit forward so you can actually see it
+        transform.position += transform.forward * forwardOffset;
     }
 }
