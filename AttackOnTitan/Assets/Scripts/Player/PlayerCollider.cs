12fc623346f95f458b0e32a293624dac72d0c63d
Dylan Laptop <momocreator@hotmail.com>
2020-12-24 00:20:30 +0100

    Updated player colliders
    - Added colliders to head and hands. These work just as you would except (you can push yourself from walls and things, and never go inside something with your head).
    - Btw, I forgot to mention it in previous update, but we know have knuckle support :)

---
+++ AttackOnTitan/Assets/Scripts/Player/PlayerCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Transform player;
+    [SerializeField] Transform playerHead;
+    [SerializeField] CapsuleCollider col;
+
+    [Header("Parameters")]
+    [SerializeField] float neckRadius = 0.2f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        col.center = new Vector3(playerHead.localPosition.x -player.InverseTransformDirection(playerHead.up).x * neckRadius,
+            playerHead.localPosition.y / 2,
+            playerHead.localPosition.z - player.InverseTransformDirection(playerHead.up).z * neckRadius);
+        col.height = playerHead.localPosition.y;
+    }
+}
