cb7ca125bff635f55561c5d5480a8c2bf2ac736f
Dylan Laptop <momocreator@hotmail.com>
2020-12-24 23:50:56 +0100

    Updated VR IK and player collosion
    - VR IK now is animated properly. Good enough for now at least.
    - Player now also has colliders on his head and hands (which makes it possible to do things like giving headbutts to the wall) and the collider follows your head and height (making it possible to crouch below things)

---
+++ AttackOnTitan/Assets/Scripts/Player/PelvisTarget.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PelvisTarget : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Transform player;
+    [SerializeField] Transform playerHead;
+
+    [Header("Parameters")]
+    [SerializeField] float neckRadius = 0.2f;
+    [SerializeField] float heightRatio = 0.5f;
+
+    private Vector3 forward;
+    private Vector3 pForward;
+    private Vector3 pUp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Position
+        transform.localPosition = new Vector3(playerHead.localPosition.x - player.InverseTransformDirection(playerHead.up).x * neckRadius,
+            playerHead.localPosition.y * heightRatio,
+            playerHead.localPosition.z - player.InverseTransformDirection(playerHead.up).z * neckRadius);
+
+        //Rotation
+        pForward = player.InverseTransformDirection(playerHead.forward);
+        pUp = player.InverseTransformDirection(playerHead.up);
+        forward = pForward;
+        forward.x += pUp.x * -pForward.y;
+        forward.z += pUp.z * -pForward.y;
+        forward.y = 0;
+        transform.localRotation = Quaternion.LookRotation(forward, Vector3.up);
+    }
+}
--- AttackOnTitan/Assets/Scripts/Player/PlayerController.cs
+++ AttackOnTitan/Assets/Scripts/Player/PlayerController.cs
@@ -10,6 +10,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject playerHead;
     private Rigidbody rb;
     private Collider col;
+    [SerializeField] private RootMotion.FinalIK.VRIK vrik;
+    [SerializeField] private Transform pelvis; //Note: refers to pelvisParent, not pelvisTarget;
+    [SerializeField] private Transform llmt; // Left leg movable target
+    [SerializeField] private Transform rlmt; //right leg movable target
+    [SerializeField] private Transform leftLegTarget;
+    [SerializeField] private Transform rightLegTarget;
 
     [Header("(Optional) Time Controller")]
     [Tooltip("If a Time Controller is applied to the rigid body, add it here so the calculations will still be valid.")]
@@ -25,6 +31,8 @@ public class PlayerController : MonoBehaviour
     
     [Header("Calculation vars")]
     internal Vector3 walkingVector3;
+    private Vector2 rotationVector;
+    float groundDistance;
     #endregion
 
     private void Start()
@@ -45,13 +53,28 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(IsGrounded()) WalkAndSlideForce();
+        groundDistance = DistToGround(10);
 
         //player rotation
         if (SteamVR_Actions.player_controller.TP_right_press.state)
         {
-            Vector2 rotationVector = SteamVR_Actions.player_controller.TP_right_vector.axis;
+            rotationVector = SteamVR_Actions.player_controller.TP_right_vector.axis;
             transform.Rotate(0, rotationVector.x * rotationMultiplier * Time.deltaTime, 0);
+
+            if(groundDistance > 0.25f) //IK should rotate on it's own on ground, but not in mid-air
+            {
+                vrik.solver.AddPlatformMotion(rb.velocity * TimeFunctions.DeltaTime(timeController),
+                Quaternion.Euler(0, rotationVector.x * rotationMultiplier * Time.deltaTime, 0), playerHead.transform.position);
+            }
+        }
+
+        if (groundDistance <= 0.25f)
+        {
+            WalkAndSlideForce(); //Note: This function will also manage IK
+        }
+        else
+        {
+            IK_Fly();
         }
     }
 
@@ -61,21 +84,41 @@ public class PlayerController : MonoBehaviour
         return Physics.Raycast(transform.position + new Vector3(0, 0.25f, 0), Vector3.down, DistanceToTheGround + 0.1f);
     }
 
+    internal float DistToGround(float maxRange)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + new Vector3(0, playerHead.transform.localPosition.y, 0), Vector3.down, out hit, maxRange, LayerMask.GetMask("Default")))
+        {
+            return hit.distance - playerHead.transform.localPosition.y;
+        }
+        else
+        {
+            return float.PositiveInfinity;
+        }
+    }
+
     private void WalkAndSlideForce()
     {
         UpdateWalkingVector();
 
-        if (SteamVR_Actions.player_controller.TP_left_press.state)
+        if (SteamVR_Actions.player_controller.TP_left_press.state && TimeFunctions.GetRealVelocity(timeController, rb).magnitude <= maxWalkSpeed * walkingVector3.magnitude)
         {
-
-            if (TimeFunctions.GetRealVelocity(timeController, rb).magnitude < maxWalkSpeed * walkingVector3.magnitude)
-                rb.AddForce(walkingVector3 * walkingForce);
-            else
-                rb.AddForce(TimeFunctions.GetRealVelocity(timeController, rb).normalized * groundResistance * -1);
-
+            rb.AddForce(walkingVector3 * walkingForce);
         }
         else
+        {
             rb.AddForce(TimeFunctions.GetRealVelocity(timeController, rb).normalized * groundResistance * -1);
+        }
+
+        //for IK
+        if (TimeFunctions.GetRealVelocity(timeController, rb).magnitude > maxWalkSpeed * 1.1f)
+        {
+            IK_Slide();
+        }
+        else
+        {
+            IK_Walk();
+        }
     }
 
     internal void UpdateWalkingVector()
@@ -85,5 +128,67 @@ public class PlayerController : MonoBehaviour
         Vector2 playerRight = new Vector2(playerHead.transform.right.x, playerHead.transform.right.z);
         Vector2 walkingVector = trackPadVector.y * playerForward + trackPadVector.x * playerRight;
         walkingVector3 = new Vector3(walkingVector.x, 0, walkingVector.y);
+    }
+
+    internal void ControlIK(float locoWeight, float posWeight, float rotWeight)
+    {
+        vrik.solver.locomotion.weight = locoWeight;// Mathf.Lerp(vrik.solver.locomotion.weight, locoWeight, 10 * TimeFunctions.DeltaTime(timeController));
+        if (locoWeight == 0)
+            vrik.solver.Reset();
+
+
+        vrik.solver.leftLeg.positionWeight = Mathf.Lerp(vrik.solver.leftLeg.positionWeight, posWeight, 100 * TimeFunctions.DeltaTime(timeController));
+        vrik.solver.rightLeg.positionWeight = Mathf.Lerp(vrik.solver.rightLeg.positionWeight, posWeight, 100 * TimeFunctions.DeltaTime(timeController));
+        vrik.solver.leftLeg.rotationWeight = Mathf.Lerp(vrik.solver.leftLeg.rotationWeight, rotWeight, 100 * TimeFunctions.DeltaTime(timeController));
+        vrik.solver.rightLeg.rotationWeight = Mathf.Lerp(vrik.solver.rightLeg.rotationWeight, rotWeight, 100 * TimeFunctions.DeltaTime(timeController));
+
+        llmt.rotation = leftLegTarget.rotation;
+        rlmt.rotation = rightLegTarget.rotation;
+    }
+
+    internal Vector3 leftLocalHip()
+    {
+        return -transform.InverseTransformDirection(pelvis.right) * 0.1f + new Vector3(pelvis.localPosition.x, 0, pelvis.localPosition.z);
+    }
+
+    internal Vector3 rightLocalHip()
+    {
+        return transform.InverseTransformDirection(pelvis.right) * 0.1f + new Vector3(pelvis.localPosition.x, 0, pelvis.localPosition.z);
+    }
+
+    internal Vector3 locRBvelocity()
+    {
+        return transform.InverseTransformDirection(TimeFunctions.GetRealVelocity(timeController, rb));
+    }
+
+    internal void IK_Walk()
+    {
+        ControlIK(1, 0, 0);
+    }
+
+    internal void IK_Slide()
+    {
+        Vector3 localRBvelocity = locRBvelocity();
+
+        llmt.localPosition = new Vector3(localRBvelocity.x, 0, localRBvelocity.z).normalized
+            * Mathf.Clamp(localRBvelocity.magnitude - maxWalkSpeed, 1, 10) * 0.05f + leftLocalHip();
+
+        rlmt.localPosition = new Vector3(localRBvelocity.x, 0, localRBvelocity.z).normalized
+            * Mathf.Clamp(localRBvelocity.magnitude - maxWalkSpeed, 1, 10) * 0.05f + rightLocalHip();
+
+        //vrik.solver.Reset();
+        ControlIK(0, 1, 0);
+    }
+
+    internal void IK_Fly()
+    {
+        float t = 0.1f / groundDistance;
+        llmt.localPosition = Vector3.Lerp(transform.InverseTransformPoint(leftLegTarget.position), leftLocalHip() + (Vector3.down + locRBvelocity()).normalized, t);
+
+        rlmt.localPosition = Vector3.Lerp(transform.InverseTransformPoint(rightLegTarget.position), rightLocalHip() + (Vector3.down + locRBvelocity()).normalized, t);
+
+        vrik.solver.AddPlatformMotion(rb.velocity * TimeFunctions.DeltaTime(timeController), Quaternion.identity, playerHead.transform.position);
+        //vrik.solver.Reset();
+        ControlIK(0, 1, Mathf.Clamp01(1 - t));
     }
 }
